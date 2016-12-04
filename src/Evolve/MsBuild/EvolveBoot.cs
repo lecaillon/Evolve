d4ad03016efdd1cce35f4961df88f108dc8903d9
lecaillon <lecaillon@hotmail.com>
2016-12-04 23:40:48 +0100

    Add EvolveBoot MsBuild Task

---
+++ src/Evolve/MsBuild/EvolveBoot.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace Evolve.MsBuild
+{
+    public class EvolveBoot : Task
+    {
+        public string ProjectOutputPath { get; set; }
+
+        public override bool Execute()
+        {
+            Log.LogMessage(MessageImportance.High, "PSG !!!");
+            Log.LogMessage(MessageImportance.High, ProjectOutputPath);
+
+            return true;
+        }
+    }
+}
---
+++ test/Evolve.MsBuild.Test/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Evolve.MsBuild.Test
+{
+    /// <summary>
+    /// msbuild /t:Build
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
