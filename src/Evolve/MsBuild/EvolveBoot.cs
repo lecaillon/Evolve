b71268c33aab5b19c41584107c297da28f94bae2
lecaillon <lecaillon@hotmail.com>
2016-12-06 08:25:45 +0100

    Messing around with Task configuration

--- src/Evolve/MsBuild/EvolveBoot.cs
+++ src/Evolve/MsBuild/EvolveBoot.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Configuration;
+using System.IO;
 using Microsoft.Build.Framework;
 using Microsoft.Build.Utilities;
 
@@ -6,14 +8,27 @@ namespace Evolve.MsBuild
 {
     public class EvolveBoot : Task
     {
-        public string ProjectOutputPath { get; set; }
+        public string TargetPath { get; set; }
 
         public override bool Execute()
         {
-            Log.LogMessage(MessageImportance.High, "PSG !!!");
-            Log.LogMessage(MessageImportance.High, ProjectOutputPath);
+            Log.LogMessage(MessageImportance.High, "");
+            Log.LogMessage(MessageImportance.High, "Start Task");
+            Log.LogMessage(MessageImportance.High, TargetPath);
+
+            string configPath = Directory.GetFiles(Path.GetDirectoryName(TargetPath), "*.config")[0];
+            Log.LogMessage(MessageImportance.High, configPath);
+
+            // http://stackoverflow.com/questions/8320543/cant-cast-to-my-custom-configurationsection
+            var fileMap = new ExeConfigurationFileMap() { ExeConfigFilename = configPath };
+            var config = ConfigurationManager.OpenMappedExeConfiguration(fileMap, ConfigurationUserLevel.None);
+            Log.LogMessage(MessageImportance.High, config.AppSettings.Settings["Test"].Value);
+
+            Log.LogMessage(MessageImportance.High, "End Task");
+            Log.LogMessage(MessageImportance.High, "");
 
             return true;
         }
+
     }
 }
--- src/Evolve/Properties/AssemblyInfo.cs
+++ src/Evolve/Properties/AssemblyInfo.cs
@@ -1,19 +1,35 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
+// Les informations générales relatives à un assembly dépendent de 
+// l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
+// associées à un assembly.
+[assembly: AssemblyTitle("Evolve")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Evolve")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
+// L'affectation de la valeur false à ComVisible rend les types invisibles dans cet assembly 
+// aux composants COM.  Si vous devez accéder à un type dans cet assembly à partir de 
+// COM, affectez la valeur true à l'attribut ComVisible sur ce type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("4cce0dbd-d5b5-419d-a6f5-df0d9544aa7f")]
+// Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
+[assembly: Guid("26876d14-0eaa-49d7-a838-b5eec9057fb6")]
+
+// Les informations de version pour un assembly se composent des quatre valeurs suivantes :
+//
+//      Version principale
+//      Version secondaire 
+//      Numéro de build
+//      Révision
+//
+// Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut 
+// en utilisant '*', comme indiqué ci-dessous :
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
