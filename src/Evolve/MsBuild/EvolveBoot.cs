cd2492002cf5e1347d06e75b5894bf7cb21728e3
Philippe Lécaillon <lecaillon@hotmail.com>
2017-01-11 10:06:39 +0100

    Add MsBuild task

--- src/Evolve/MsBuild/EvolveBoot.cs
+++ src/Evolve/MsBuild/EvolveBoot.cs
@@ -1,36 +1,27 @@
-﻿//using System;
-//using System.IO;
-//using Evolve.Driver;
-//using Microsoft.Build.Framework;
-//using Microsoft.Build.Utilities;
+﻿using System;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
 
-//namespace Evolve.MsBuild
-//{
-//    public class EvolveBoot : Task
-//    {
-//        public string TargetPath { get; set; }
+namespace Evolve.MsBuild
+{
+    public class EvolveBoot : Task
+    {
+        public string TargetPath { get; set; }
 
-//        public override bool Execute()
-//        {
-//            Log.LogMessage(MessageImportance.High, "");
-//            Log.LogMessage(MessageImportance.High, "Start Task");
-//            Log.LogMessage(MessageImportance.High, TargetPath);
+        public override bool Execute()
+        {
+            Log.LogMessage(MessageImportance.High, "");
+            Log.LogMessage(MessageImportance.High, "Start EvolveBoot task");
+            Log.LogMessage(MessageImportance.High, TargetPath);
 
-//            //string configPath = Directory.GetFiles(Path.GetDirectoryName(TargetPath), "*.config")[0];
-//            //Log.LogMessage(MessageImportance.High, configPath);
+            Log.LogMessage(MessageImportance.High, "End EvolveBoot task");
+            Log.LogMessage(MessageImportance.High, "");
 
-//            // http://stackoverflow.com/questions/8320543/cant-cast-to-my-custom-configurationsection
-//            //var fileMap = new ExeConfigurationFileMap() { ExeConfigFilename = configPath };
-//            //var config = ConfigurationManager.OpenMappedExeConfiguration(fileMap, ConfigurationUserLevel.None);
-//            //Log.LogMessage(MessageImportance.High, config.AppSettings.Settings["Test"].Value);
+            return true;
+        }
+    }
+}
 
-//            Log.LogMessage(MessageImportance.High, "End Task");
-//            Log.LogMessage(MessageImportance.High, "");
-
-//            return true;
-//        }
-//    }
-//}
 
 
 
