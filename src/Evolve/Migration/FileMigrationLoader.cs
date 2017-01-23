b0963abc1d95c3c539d0d488900fa3abbbe56f66
Philippe Lécaillon <lecaillon@hotmail.com>
2017-01-24 00:40:00 +0100

    Start implementing IMigrationLoader

--- src/Evolve/Migration/FileMigrationLoader.cs
+++ src/Evolve/Migration/FileMigrationLoader.cs
@@ -1,12 +1,15 @@
-﻿using System;
+﻿using Evolve.Utilities;
+using System;
 using System.Collections.Generic;
 
 namespace Evolve.Migration
 {
     public class FileMigrationLoader : IMigrationLoader
     {
-        public IEnumerable<MigrationScript> GetMigrations(IEnumerable<string> Locations)
+        public IEnumerable<MigrationScript> GetMigrations(IEnumerable<string> locations)
         {
+            Check.HasNoNulls(locations, nameof(locations));
+
             throw new NotImplementedException();
         }
     }
