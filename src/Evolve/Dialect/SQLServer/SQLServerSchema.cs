27a4eb23ce3b5f2800a841f28be9710188a7a961
Philippe Lécaillon <lecaillon@hotmail.com>
2017-01-27 01:16:37 +0100

    Db / Dialect extensions

--- src/Evolve/Dialect/SQLServer/SQLServerSchema.cs
+++ src/Evolve/Dialect/SQLServer/SQLServerSchema.cs
@@ -4,7 +4,36 @@ using System.Text;
 
 namespace Evolve.Dialect.SQLServer
 {
-    class SQLServerSchema
+    public class SQLServerSchema : Schema
     {
+        public SQLServerSchema(string schemaName, DatabaseHelper databaseHelper) : base(schemaName, databaseHelper)
+        {
+        }
+
+        protected override bool InternalIsExists()
+        {
+            // SELECT COUNT(*) FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME=?
+            throw new NotImplementedException();
+        }
+
+        protected override bool InternalClean()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override bool InternalCreate()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override bool InternalDrop()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override bool InternalIsEmpty()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
