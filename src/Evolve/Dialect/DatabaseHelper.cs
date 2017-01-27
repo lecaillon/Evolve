bba7fed60dd2f20253be55c9371292a614029090
Philippe Lécaillon <lecaillon@hotmail.com>
2017-01-27 23:06:26 +0100

    Add Query helpers

--- src/Evolve/Dialect/DatabaseHelper.cs
+++ src/Evolve/Dialect/DatabaseHelper.cs
@@ -1,4 +1,7 @@
-﻿using Evolve.Connection;
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using Evolve.Connection;
 using Evolve.Utilities;
 
 namespace Evolve.Dialect
@@ -15,7 +18,11 @@ namespace Evolve.Dialect
 
         public IConnectionProvider ConnectionProvider { get; private set; }
 
+        protected IDbConnection Connection => ConnectionProvider.GetConnection();
+
         public abstract string DatabaseName { get; protected set; }
+
+        #region Schema helper
 
         public string OriginalSchemaName { get; private set; }
 
@@ -36,5 +43,103 @@ namespace Evolve.Dialect
         protected abstract string InternalGetCurrentSchemaName();
 
         protected abstract string InternalRestoreCurrentSchema();
+
+        #endregion
+
+        #region Query helper
+
+        public virtual long QueryForLong(string sql) => (long)ExecuteScalar(Connection, sql);
+
+        public virtual string QueryForString(string sql) => (string)ExecuteScalar(Connection, sql);
+
+        public virtual IEnumerable<string> QueryForListOfString(string sql)
+        {
+            var list = new List<string>();
+            using (var reader = (IDataReader)ExecuteReader(Connection, sql))
+            {
+                while (reader.Read())
+                {
+                    list.Add(reader[0] is DBNull ? null : reader[0].ToString());
+                }
+            }
+
+            return list;
+        }
+
+        protected int ExecuteNonQuery(IDbConnection connection, string sql, IDbTransaction transaction = null)
+            => (int)Execute(connection, sql, transaction, nameof(ExecuteNonQuery));
+
+        protected object ExecuteScalar(IDbConnection connection, string sql, IDbTransaction transaction = null)
+            => Execute(connection, sql, transaction, nameof(ExecuteScalar));
+
+        protected object ExecuteReader(IDbConnection connection, string sql, IDbTransaction transaction = null)
+            => Execute(connection, sql, transaction, nameof(ExecuteReader));
+
+        protected object Execute(IDbConnection connection, string sql, IDbTransaction transaction, string executeMethod)
+        {
+            Check.NotNull(connection, nameof(connection));
+            Check.NotNullOrEmpty(sql, nameof(sql));
+            Check.NotNullOrEmpty(executeMethod, nameof(executeMethod));
+
+            bool wasClosed = connection.State == ConnectionState.Closed;
+            var dbCommand = connection.CreateCommand();
+            dbCommand.CommandText = sql;
+            dbCommand.Transaction = transaction;
+
+            object result;
+            try
+            {
+                if (wasClosed) connection.Open();
+
+                switch (executeMethod)
+                {
+                    case nameof(ExecuteNonQuery):
+                    {
+                        using (dbCommand)
+                        {
+                            result = dbCommand.ExecuteNonQuery();
+                        }
+
+                        break;
+                    }
+                    case nameof(ExecuteScalar):
+                    {
+                        using (dbCommand)
+                        {
+                            result = dbCommand.ExecuteScalar();
+                        }
+
+                        break;
+                    }
+                    case nameof(ExecuteReader):
+                    {
+                        using (dbCommand)
+                        {
+                            result = dbCommand.ExecuteReader();
+                        }
+
+                        break;
+                    }
+                    default:
+                    {
+                        throw new NotSupportedException();
+                    }
+                }
+
+                if (wasClosed)
+                {
+                    connection.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                connection.Close();
+                throw new EvolveException("", ex);
+            }
+
+            return result;
+        }
+
+        #endregion
     }
 }
