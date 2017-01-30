5ccb07e387665d773b2d0eb7b265919dc3c10e0e
Philippe Lécaillon <lecaillon@hotmail.com>
2017-01-30 01:11:54 +0100

    Add Sqlite + unit test DbConnectionExtensions

---
+++ test/Evolve.Test/Extensions/DbConnectionExtensionsTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Data;
+using Evolve.Extensions;
+using Microsoft.Data.Sqlite;
+using Xunit;
+
+namespace Evolve.Test.Extensions
+{
+    public class DbConnectionExtensionsTest
+    {
+        [Fact(DisplayName = "QueryForLong works")]
+        public void QueryForLong_works()
+        {
+            using (var connection = new SqliteConnection("Data Source=:memory:"))
+            {
+                Assert.Equal(1L, DbConnectionExtensions.QueryForLong(connection, "SELECT 1;"));
+                Assert.True(connection.State == ConnectionState.Closed);
+            }
+        }
+    }
+}
