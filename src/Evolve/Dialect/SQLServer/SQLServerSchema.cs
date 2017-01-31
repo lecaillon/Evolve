b14d61ad23ed7ac75d4611bd59fcb544342f736f
plecaillon <plecaillon@mousquetaires.com>
2017-01-31 17:27:01 +0100

    Implementation of SQLServerSchema

--- src/Evolve/Dialect/SQLServer/SQLServerSchema.cs
+++ src/Evolve/Dialect/SQLServer/SQLServerSchema.cs
@@ -10,7 +10,7 @@ namespace Evolve.Dialect.SQLServer
 
         protected override bool IsExists()
         {
-            string sql = $"SELECT COUNT(*) FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = '{Name}';";
+            string sql = $"SELECT COUNT(*) FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = '{Name}'";
             return _wrappedConnection.DbConnection.QueryForLong(sql) > 0;
         }
 
@@ -33,7 +33,7 @@ namespace Evolve.Dialect.SQLServer
 
         protected override bool Create()
         {
-            string sql = $"CREATE SCHEMA '{Name}'";
+            string sql = $"CREATE SCHEMA [{Name}]";
             _wrappedConnection.DbConnection.ExecuteNonQuery(sql, _wrappedConnection.CurrentTx);
 
             return true;
@@ -43,7 +43,7 @@ namespace Evolve.Dialect.SQLServer
         {
             Clean();
 
-            string sql = $"DROP SCHEMA '{Name}'";
+            string sql = $"DROP SCHEMA [{Name}]";
             _wrappedConnection.DbConnection.ExecuteNonQuery(sql, _wrappedConnection.CurrentTx);
 
             return true;
@@ -56,6 +56,10 @@ namespace Evolve.Dialect.SQLServer
             CleanProcedures();
             CleanViews();
             CleanTables();
+            CleanFunctions();
+            CleanTypes();
+            CleanSynonyms();
+            CleanSequences(); // SELECT CAST(LEFT(CAST(SERVERPROPERTY ('productversion') as VARCHAR), 2) as int)
 
             return true;
         }
@@ -63,13 +67,13 @@ namespace Evolve.Dialect.SQLServer
         private void CleanForeignKeys()
         {
             string sql = "SELECT TABLE_NAME, CONSTRAINT_NAME " + 
-                         "FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS " + 
-                         "WHERE TYPE IN ('FOREIGN KEY','CHECK') " +
+                         "FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS " +
+                         "WHERE CONSTRAINT_TYPE IN ('FOREIGN KEY','CHECK') " +
                         $"AND TABLE_SCHEMA = '{Name}'";
 
             _wrappedConnection.DbConnection.QueryForListOfT(sql, (r) => new { Table = r.GetString(0), Constraint = r.GetString(1) }).ToList().ForEach(x =>
             {
-                string drop = $"ALTER TABLE '{Name}.{x.Table}' DROP CONSTRAINT '{x.Constraint}';";
+                string drop = $"ALTER TABLE [{Name}].[{x.Table}] DROP CONSTRAINT [{x.Constraint}]";
                 _wrappedConnection.DbConnection.ExecuteNonQuery(drop, _wrappedConnection.CurrentTx);
             });
         }
@@ -84,34 +88,79 @@ namespace Evolve.Dialect.SQLServer
 
             _wrappedConnection.DbConnection.QueryForListOfT(sql, (r) => new { Table = r.GetString(0), Constraint = r.GetString(1) }).ToList().ForEach(x =>
             {
-                string drop = $"ALTER TABLE '{Name}.{x.Table}' DROP CONSTRAINT '{x.Constraint}';";
+                string drop = $"ALTER TABLE [{Name}].[{x.Table}] DROP CONSTRAINT [{x.Constraint}]";
                 _wrappedConnection.DbConnection.ExecuteNonQuery(drop, _wrappedConnection.CurrentTx);
             });
         }
 
         private void CleanProcedures()
         {
-            string sql = $"SELECT routine_name FROM INFORMATION_SCHEMA.ROUTINES WHERE routine_schema = '{Name}' AND routine_type = 'PROCEDURE' ORDER BY created DESC;";
+            string sql = $"SELECT routine_name FROM INFORMATION_SCHEMA.ROUTINES WHERE routine_schema = '{Name}' AND routine_type = 'PROCEDURE' ORDER BY created DESC";
             _wrappedConnection.DbConnection.QueryForListOfString(sql).ToList().ForEach(proc =>
             {
-                string drop = $"DROP PROCEDURE '{Name}.{proc}';";
+                string drop = $"DROP PROCEDURE [{Name}].[{proc}]";
                 _wrappedConnection.DbConnection.ExecuteNonQuery(drop, _wrappedConnection.CurrentTx);
             });
         }
 
         private void CleanViews()
         {
-            string sql = $"SELECT table_name FROM INFORMATION_SCHEMA.VIEWS WHERE table_schema = '{Name}';";
+            string sql = $"SELECT table_name FROM INFORMATION_SCHEMA.VIEWS WHERE table_schema = '{Name}'";
             _wrappedConnection.DbConnection.QueryForListOfString(sql).ToList().ForEach(vw =>
             {
-                string drop = $"DROP VIEW '{Name}.{vw}';";
+                string drop = $"DROP VIEW [{Name}].[{vw}]";
                 _wrappedConnection.DbConnection.ExecuteNonQuery(drop, _wrappedConnection.CurrentTx);
             });
         }
 
         private void CleanTables()
         {
-            // TODO
+            string sql = $"SELECT table_name FROM INFORMATION_SCHEMA.TABLES WHERE table_type='BASE TABLE' AND table_schema = '{Name}'";
+            _wrappedConnection.DbConnection.QueryForListOfString(sql).ToList().ForEach(t =>
+            {
+                string drop = $"DROP TABLE [{Name}].[{t}]";
+                _wrappedConnection.DbConnection.ExecuteNonQuery(drop, _wrappedConnection.CurrentTx);
+            });
+        }
+
+        private void CleanFunctions()
+        {
+            string sql = $"SELECT routine_name FROM INFORMATION_SCHEMA.ROUTINES WHERE routine_schema = '{Name}' AND routine_type = 'FUNCTION' ORDER BY created DESC";
+            _wrappedConnection.DbConnection.QueryForListOfString(sql).ToList().ForEach(fn =>
+            {
+                string drop = $"DROP FUNCTION [{Name}].[{fn}]";
+                _wrappedConnection.DbConnection.ExecuteNonQuery(drop, _wrappedConnection.CurrentTx);
+            });
+        }
+
+        private void CleanTypes()
+        {
+            string sql = $"SELECT t.name FROM sys.types t INNER JOIN sys.schemas s ON t.schema_id = s.schema_id WHERE t.is_user_defined = 1 AND s.name = '{Name}'";
+            _wrappedConnection.DbConnection.QueryForListOfString(sql).ToList().ForEach(t =>
+            {
+                string drop = $"DROP TYPE [{Name}].[{t}]";
+                _wrappedConnection.DbConnection.ExecuteNonQuery(drop, _wrappedConnection.CurrentTx);
+            });
+        }
+
+        private void CleanSynonyms()
+        {
+            string sql = $"SELECT sn.name FROM sys.synonyms sn INNER JOIN sys.schemas s ON sn.schema_id = s.schema_id WHERE s.name = '{Name}'";
+            _wrappedConnection.DbConnection.QueryForListOfString(sql).ToList().ForEach(s =>
+            {
+                string drop = $"DROP SYNONYM [{Name}].[{s}]";
+                _wrappedConnection.DbConnection.ExecuteNonQuery(drop, _wrappedConnection.CurrentTx);
+            });
+        }
+
+        private void CleanSequences()
+        {
+            string sql = $"SELECT sequence_name FROM INFORMATION_SCHEMA.SEQUENCES WHERE sequence_schema = '{Name}'";
+            _wrappedConnection.DbConnection.QueryForListOfString(sql).ToList().ForEach(s =>
+            {
+                string drop = $"DROP SEQUENCE [{Name}].[{s}]";
+                _wrappedConnection.DbConnection.ExecuteNonQuery(drop, _wrappedConnection.CurrentTx);
+            });
         }
     }
 }
