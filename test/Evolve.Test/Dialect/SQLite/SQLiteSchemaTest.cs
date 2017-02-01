debec22623415d0bbe2ddb0c90fb1deed00e875a
Philippe Lécaillon <lecaillon@hotmail.com>
2017-02-01 01:51:29 +0100

    Qtart testing SQLiteSchemaTest

---
+++ test/Evolve.Test/Dialect/SQLite/SQLiteSchemaTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Microsoft.Data.Sqlite;
+using Xunit;
+
+namespace Evolve.Test.Dialect.SQLite
+{
+    public class SQLiteSchemaTest
+    {
+        [Fact(DisplayName = "blah")]
+        public void blah()
+        {
+            string script = File.ReadAllText(@"C:\Users\Phil-Dev\Downloads\ChinookDatabase1.4_Sqlite\Chinook_Sqlite_AutoIncrementPKs.sql");
+
+            using (var connection = new SqliteConnection("Data Source=:memory:"))
+            {
+                connection.Open();
+                using (var command = connection.CreateCommand())
+                {
+                    command.CommandText = script;
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
