f8d9a7c21332f33840d23e9666cb5dae44fcfd4d
Philippe Lécaillon <lecaillon@hotmail.com>
2017-02-03 23:31:37 +0100

    Start implementing Metadatable

--- src/Evolve/Metadata/IEvolveMetadata.cs
+++ src/Evolve/Metadata/IEvolveMetadata.cs
@@ -5,6 +5,10 @@ namespace Evolve.Metadata
 {
     public interface IEvolveMetadata
     {
+        void Lock();
+
+        bool CreateIfNotExists();
+
         MigrationMetadata AddMigrationMetadata(MigrationScript migration);
 
         IEnumerable<MigrationMetadata> GetAllMigrationMetadata();
--- src/Evolve/Metadata/MetadataTable.cs
+++ src/Evolve/Metadata/MetadataTable.cs
@@ -1,26 +1,36 @@
 ﻿using Evolve.Migration;
 using System.Collections.Generic;
+using Evolve.Utilities;
 
 namespace Evolve.Metadata
 {
     public abstract class MetadataTable : IEvolveMetadata
     {
+        public MetadataTable(string schema)
+        {
+            Schema = Check.NotNullOrEmpty(schema, nameof(schema));
+        }
+
+        public string Schema { get; private set; }
+
+        public abstract void Lock();
+
+        public abstract bool CreateIfNotExists();
+
         public MigrationMetadata AddMigrationMetadata(MigrationScript migration)
         {
-            CreateTableIfNotExists();
+            CreateIfNotExists();
             return InternalAddMigrationMetadata(migration);
         }
 
         public IEnumerable<MigrationMetadata> GetAllMigrationMetadata()
         {
-            CreateTableIfNotExists();
+            CreateIfNotExists();
             return InternalGetAllMigrationMetadata();
         }
 
         protected abstract MigrationMetadata InternalAddMigrationMetadata(MigrationScript migration);
 
         protected abstract IEnumerable<MigrationMetadata> InternalGetAllMigrationMetadata();
-
-        protected abstract void CreateTableIfNotExists();
     }
 }
