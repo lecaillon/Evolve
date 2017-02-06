b3afdb6c151b479d4f5e6feb32d43600345c677f
Philippe Lécaillon <lecaillon@hotmail.com>
2017-02-07 00:08:31 +0100

    Begin SQLiteDatabase unit tests

---
+++ test/Evolve.Test/Dialect/SQLite/SQLiteDatabaseTest.cs
@@ -0,0 +1,18 @@
+﻿using Evolve.Dialect.SQLite;
+using Xunit;
+
+namespace Evolve.Test.Dialect.SQLite
+{
+    public class SQLiteDatabaseTest
+    {
+        [Fact(DisplayName = "SQLiteDatabase_name_is_sqlite")]
+        public void SQLiteDatabase_name_is_sqlite()
+        {
+            using (var connection = TestUtil.GetInMemorySQLiteWrappedConnection())
+            {
+                var db = new SQLiteDatabase(connection);
+                Assert.Equal("sqlite", db.DatabaseName);
+            }
+        }
+    }
+}
