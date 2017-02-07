46e6333a6a3d81fa300f9824da39180cab3d4c2c
Philippe Lécaillon <lecaillon@hotmail.com>
2017-02-08 00:28:20 +0100

    Add SQLiteDatabase unit tests

--- test/Evolve.Test/Dialect/SQLite/SQLiteDatabaseTest.cs
+++ test/Evolve.Test/Dialect/SQLite/SQLiteDatabaseTest.cs
@@ -14,5 +14,41 @@ namespace Evolve.Test.Dialect.SQLite
                 Assert.Equal("sqlite", db.DatabaseName);
             }
         }
+
+        [Fact(DisplayName = "GetCurrentSchemaName_is_always_main")]
+        public void GetCurrentSchemaName_is_always_main()
+        {
+            using (var connection = TestUtil.GetInMemorySQLiteWrappedConnection())
+            {
+                var db = new SQLiteDatabase(connection);
+                Assert.Equal("main", db.GetCurrentSchemaName());
+            }
+        }
+
+        [Fact(DisplayName = "ChangeSchema_always_returns_main")]
+        public void ChangeSchema_always_returns_main()
+        {
+            using (var connection = TestUtil.GetInMemorySQLiteWrappedConnection())
+            {
+                var db = new SQLiteDatabase(connection);
+                var schema = db.ChangeSchema("another_shema");
+
+                Assert.NotNull(schema);
+                Assert.Equal("main", schema.Name);
+            }
+        }
+
+        [Fact(DisplayName = "GetMetadataTable_works")]
+        public void GetMetadataTable_works()
+        {
+            using (var connection = TestUtil.GetInMemorySQLiteWrappedConnection())
+            {
+                var db = new SQLiteDatabase(connection);
+                var metadataTable = db.GetMetadataTable("main", TestContext.DefaultMetadataTableName);
+
+                Assert.NotNull(metadataTable);
+                Assert.True(metadataTable.CreateIfNotExists());
+            }
+        }
     }
 }
