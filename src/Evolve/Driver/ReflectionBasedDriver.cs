dece0f9335b9fd36e9b2723f0b707d2a35c1914e
Philippe Lécaillon <lecaillon@hotmail.com>
2017-02-12 23:23:55 +0100

    Implementation of the .NET ReflectionBasedDriver

--- src/Evolve/Driver/ReflectionBasedDriver.cs
+++ src/Evolve/Driver/ReflectionBasedDriver.cs
@@ -1,110 +1,101 @@
 ﻿using Evolve.Utilities;
 using System;
 using System.Data;
+using System.Reflection;
 
 namespace Evolve.Driver
 {
     public abstract class ReflectionBasedDriver : IDriver
     {
-        protected const string IDbConnectionImplementationNotFound = "The IDbConnection implementation in the assembly {0} could not be found. Ensure that the assembly is located in the application directory.";
-
+#if NETSTANDARD
+        protected const string IDbConnectionImplementationNotFound = "";
+#else
+        protected const string IDbConnectionImplementationNotFound = "The IDbConnection implementation could not be found in the assembly {0}. Ensure that the assembly is located in the application directory.";
+#endif
         /// <summary>
-        /// Initializes a new instance of <see cref="ReflectionBasedDriver" /> with
-        /// type names that are loaded from the specified assembly.
+        ///     Initializes a new instance of <see cref="ReflectionBasedDriver" /> with
+        ///     type names that are loaded from the specified assembly.
         /// </summary>
-        /// <param name="driverAssemblyName">Assembly to load the types from.</param>
-        /// <param name="connectionTypeName">Connection type name.</param>
+        /// <param name="driverAssemblyName"> Assembly to load the DbConnection type from. </param>
+        /// <param name="connectionTypeName"> DbConnection type name. </param>
         protected ReflectionBasedDriver(string driverAssemblyName, string connectionTypeName)
         {
-            ConnectionType = TypeFromAssembly(connectionTypeName, driverAssemblyName);
-
+            DriverTypeName = new AssemblyQualifiedTypeName(connectionTypeName, driverAssemblyName);
+            DbConnectionType = TypeFromLoadedAssembly();
+            if(DbConnectionType == null)
+            {
+                DbConnectionType = TypeFromAssembly();
+            }
         }
+
+        public Type DbConnectionType { get; set; }
+
+        protected AssemblyQualifiedTypeName DriverTypeName { get; }
 
         public IDbConnection CreateConnection()
         {
-            throw new NotImplementedException();
+            return (IDbConnection)Activator.CreateInstance(DbConnectionType);
         }
-
-        protected Type TypeFromAssembly(string type, string assembly)
-        {
-            return TypeFromAssembly(new AssemblyQualifiedTypeName(type, assembly));
-        }
-
-        public Type ConnectionType { get; set; }
-
-        // http://stackoverflow.com/questions/658446/how-do-i-find-the-fully-qualified-name-of-an-assembly
-        // "[System.Reflection.AssemblyName]::GetAssemblyName(\"$(TargetPath)\").FullName"
-
-        // http://stackoverflow.com/questions/37895278/how-to-load-assemblies-located-in-a-folder-in-net-core-console-app
 
         /// <summary>
-        /// Returns a <see cref="Type"/> from an already loaded Assembly or an
-        /// Assembly that is loaded with a partial name.
+        ///     Try to return a DbConnection from an already loaded assembly.
         /// </summary>
-        /// <param name="name">An <see cref="AssemblyQualifiedTypeName" />.</param>
-        /// <returns>
-        /// A <see cref="Type"/> object that represents the specified type,
-        /// or <see langword="null" /> if the type cannot be loaded.
-        /// </returns>
-        private Type TypeFromAssembly(AssemblyQualifiedTypeName name)
+        /// <returns> A DbConnection type or null. </returns>
+        private Type TypeFromLoadedAssembly()
         {
             try
             {
-                // Try to get the type from an already loaded assembly
-                Type type = Type.GetType(name.ToString());
-
-                if (type != null)
-                {
-                    return type;
-                }
-
-                //if (name.Assembly == null)
-                //{
-                //    // No assembly was specified for the type, so just fail
-                //    string message = "Could not load type " + name + ". Possible cause: no assembly name specified.";
-                //    log.Warn(message);
-                //    if (throwOnError) throw new TypeLoadException(message);
-                //    return null;
-                //}
-
-                //Assembly assembly = Assembly.Load(name.Assembly);
-
-                //if (assembly == null)
-                //{
-                //    log.Warn("Could not load type " + name + ". Possible cause: incorrect assembly name specified.");
-                //    return null;
-                //}
-
-                //type = assembly.GetType(name.Type, throwOnError);
-
-                //if (type == null)
-                //{
-                //    log.Warn("Could not load type " + name + ".");
-                //    return null;
-                //}
-
-                return type;
+                return Type.GetType(DriverTypeName.ToString());
             }
-            catch // (Exception e)
+            catch
             {
-                //if (log.IsErrorEnabled)
-                //{
-                //    log.Error("Could not load type " + name + ".", e);
-                //}
-                //if (throwOnError) throw;
                 return null;
             }
         }
 
-        private class AssemblyQualifiedTypeName
+#if NETSTANDARD
+
+        /// <summary>
+        ///     Try to return a DbConnection from an assembly.
+        /// </summary>
+        /// <returns> A DbConnection type or null. </returns>
+        protected virtual Type TypeFromAssembly()
         {
-            public AssemblyQualifiedTypeName(string type, string assembly = null)
+            throw new NotImplementedException();
+        }
+
+#else
+
+        /// <summary>
+        ///     Try to return a DbConnection from an assembly.
+        /// </summary>
+        /// <returns> A DbConnection type. </returns>
+        /// <exception cref="EvolveException"> When the DbConnection type can't be loaded. </exception>
+        protected virtual Type TypeFromAssembly()
+        {
+            try
+            {
+                var assembly = Assembly.LoadFrom(DriverTypeName.Assembly + ".dll");
+                return assembly.GetType(DriverTypeName.Type);
+            }
+            catch(Exception ex)
+            {
+                throw new EvolveException(string.Format(IDbConnectionImplementationNotFound, DriverTypeName.Assembly + ".dll"), ex);
+            }
+        }
+
+#endif
+
+        protected class AssemblyQualifiedTypeName
+        {
+            public AssemblyQualifiedTypeName(string type, string assembly)
             {
                 Type = Check.NotNullOrEmpty(type, nameof(type));
-                Assembly = assembly;
+                Assembly = Check.NotNullOrEmpty(assembly, nameof(assembly));
             }
 
             public string Type { get; }
+
             public string Assembly { get; }
 
             public override string ToString()
