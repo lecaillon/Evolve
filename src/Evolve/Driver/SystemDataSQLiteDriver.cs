2f780f04e7d707de267c5577665bb19557ecb13c
Philippe Lécaillon <lecaillon@hotmail.com>
2017-02-13 22:55:42 +0100

    Add new SQLite driver

---
+++ src/Evolve/Driver/SystemDataSQLiteDriver.cs
@@ -0,0 +1,12 @@
+﻿namespace Evolve.Driver
+{
+    public class SystemDataSQLiteDriver : ReflectionBasedDriver
+    {
+        public const string DriverAssemblyName = "System.Data.SQLite";
+        public const string ConnectionTypeName = "System.Data.SQLite.SQLiteConnection";
+
+        public SystemDataSQLiteDriver() : base(DriverAssemblyName, ConnectionTypeName)
+        {
+        }
+    }
+}
