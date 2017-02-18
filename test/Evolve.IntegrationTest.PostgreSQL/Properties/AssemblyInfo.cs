d2322221f8d7a9d69350242103f81206ec804cbc
Philippe Lécaillon <lecaillon@hotmail.com>
2017-02-18 23:50:57 +0100

    Add PostgreSQL integration tests project

---
+++ test/Evolve.IntegrationTest.PostgreSQL/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Evolve.IntegrationTest.PostgreSQL")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Evolve.IntegrationTest.PostgreSQL")]
+[assembly: AssemblyCopyright("Copyright © 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("5d083251-040d-40e6-aa6f-7ab5959e2b76")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
