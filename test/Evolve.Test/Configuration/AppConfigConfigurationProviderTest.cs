b1067dd303e0833556103cd3b608bbee9bc2d2d9
Philippe Lécaillon <lecaillon@hotmail.com>
2017-02-22 00:39:43 +0100

    Add new .NET unit test project

---
+++ test/Evolve.Test/Configuration/AppConfigConfigurationProviderTest.cs
@@ -0,0 +1,16 @@
+﻿using Evolve.Configuration;
+using Xunit;
+
+namespace Evolve.Test.Configuration
+{
+    public class AppConfigConfigurationProviderTest
+    {
+        [Fact()]
+        public void Test()
+        {
+            var configurationProvider = new AppConfigConfigurationProvider();
+            configurationProvider.Configure("", new Evolve());
+
+        }
+    }
+}
---
+++ test/Evolve.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Evolve.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Evolve.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("8cdc7d97-26f6-4ca2-9d55-0434e86664d4")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
