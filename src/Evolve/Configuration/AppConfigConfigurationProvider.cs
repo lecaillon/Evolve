9a89308936585538bfdec4908d589b54923f1533
plecaillon <plecaillon@mousquetaires.com>
2017-02-22 12:43:22 +0100

    Add comments and controls

--- src/Evolve/Configuration/AppConfigConfigurationProvider.cs
+++ src/Evolve/Configuration/AppConfigConfigurationProvider.cs
@@ -8,6 +8,7 @@ namespace Evolve.Configuration
 {
     public class AppConfigConfigurationProvider : EvolveConfigurationProviderBase
     {
+        private const string IncorrectFileFormat = "Incorrect file format: {0}.";
         private const string NotSupportedAppConfigFileName = "AppConfigConfigurationProvider is designed to manage App.Config or Web.config files only.";
         private const string IncorrectEncodingValue = "Encoding does not support this value: {0}. See https://msdn.microsoft.com/en-us/library/system.text.encoding.getencodings(v=vs.110).aspx for all possible names.";
         private const string InvalidVersionPatternMatching = "Migration version {0} is invalid. Version must respect this regex: ^[0-9]+(?:.[0-9]+)*$";
@@ -25,9 +26,19 @@ namespace Evolve.Configuration
                 ExeConfigFilename = _filePath
             };
 
-            var config = ConfigurationManager.OpenMappedExeConfiguration(configMap, ConfigurationUserLevel.None);
+            System.Configuration.Configuration config = null;
+            try
+            {
+                config = ConfigurationManager.OpenMappedExeConfiguration(configMap, ConfigurationUserLevel.None);
+            }
+            catch (Exception ex)
+            {
+                throw new EvolveConfigurationException(string.Format(IncorrectFileFormat, _filePath), ex);
+            }
+
             var appSettings = config.AppSettings.Settings;
 
+            // ConnectionString
             if (!string.IsNullOrWhiteSpace(appSettings[ConnectionString]?.Value))
             {
                 string cnn = appSettings[ConnectionString].Value;
@@ -41,16 +52,19 @@ namespace Evolve.Configuration
                 }
             }
 
+            // Driver
             if (!string.IsNullOrWhiteSpace(appSettings[Driver]?.Value))
             {
                 _configuration.Driver = appSettings[Driver].Value;
             }
 
+            // Locations
             if (!string.IsNullOrWhiteSpace(appSettings[Locations]?.Value))
             {
                 _configuration.Locations = appSettings[Locations].Value.Trim(';').Split(';');
             }
 
+            // Encoding
             if (!string.IsNullOrWhiteSpace(appSettings[Encoding]?.Value))
             {
                 try
@@ -63,46 +77,55 @@ namespace Evolve.Configuration
                 }
             }
 
+            // SqlMigrationPrefix
             if (!string.IsNullOrWhiteSpace(appSettings[SqlMigrationPrefix]?.Value))
             {
                 _configuration.SqlMigrationPrefix = appSettings[SqlMigrationPrefix].Value;
             }
 
+            // SqlMigrationSeparator
             if (!string.IsNullOrWhiteSpace(appSettings[SqlMigrationSeparator]?.Value))
             {
                 _configuration.SqlMigrationSeparator = appSettings[SqlMigrationSeparator].Value;
             }
 
+            // SqlMigrationSuffix
             if (!string.IsNullOrWhiteSpace(appSettings[SqlMigrationSuffix]?.Value))
             {
                 _configuration.SqlMigrationSuffix = appSettings[SqlMigrationSuffix].Value;
             }
 
+            // DefaultSchema
             if (!string.IsNullOrWhiteSpace(appSettings[DefaultSchema]?.Value))
             {
                 _configuration.DefaultSchema = appSettings[DefaultSchema].Value;
             }
 
+            // MetadaTableSchema
             if (!string.IsNullOrWhiteSpace(appSettings[MetadaTableSchema]?.Value))
             {
                 _configuration.MetadaTableSchema = appSettings[MetadaTableSchema].Value;
             }
 
+            // MetadaTableName
             if (!string.IsNullOrWhiteSpace(appSettings[MetadaTableName]?.Value))
             {
                 _configuration.MetadaTableName = appSettings[MetadaTableName].Value;
             }
 
+            // PlaceholderPrefix
             if (!string.IsNullOrWhiteSpace(appSettings[PlaceholderPrefix]?.Value))
             {
                 _configuration.PlaceholderPrefix = appSettings[PlaceholderPrefix].Value;
             }
 
+            // PlaceholderSuffix
             if (!string.IsNullOrWhiteSpace(appSettings[PlaceholderSuffix]?.Value))
             {
                 _configuration.PlaceholderSuffix = appSettings[PlaceholderSuffix].Value;
             }
 
+            // TargetVersion
             if (!string.IsNullOrWhiteSpace(appSettings[TargetVersion]?.Value))
             {
                 try
