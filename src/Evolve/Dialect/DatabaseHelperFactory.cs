7ce1c1bd4222d4b2bca360acf5169eed011e3fed
Philippe Lécaillon <lecaillon@hotmail.com>
2017-02-24 23:27:00 +0100

    Fix DatabaseHelperFactory

--- src/Evolve/Dialect/DatabaseHelperFactory.cs
+++ src/Evolve/Dialect/DatabaseHelperFactory.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using Evolve.Connection;
 using Evolve.Dialect.PostgreSQL;
 using Evolve.Dialect.SQLite;
+using Evolve.Utilities;
 
 namespace Evolve.Dialect
 {
     public static class DatabaseHelperFactory
     {
+        private const string UnknownDBMS = "Unknown DBMS {0}.";
+
         private static Dictionary<DBMS, Func<WrappedConnection, DatabaseHelper>> _dbmsMap = new Dictionary<DBMS, Func<WrappedConnection, DatabaseHelper>>
         {
             [DBMS.SQLite] = wcnn => new SQLiteDatabase(wcnn),
@@ -16,10 +19,12 @@ namespace Evolve.Dialect
 
         public static DatabaseHelper GetDatabaseHelper(DBMS dbmsType, WrappedConnection connection)
         {
+            Check.NotNull(connection, nameof(connection));
+
             _dbmsMap.TryGetValue(dbmsType, out Func<WrappedConnection, DatabaseHelper> dbHelperCreationDelegate);
             if(dbHelperCreationDelegate == null)
             {
-
+                throw new EvolveException(string.Format(UnknownDBMS, dbmsType));
             }
 
             return dbHelperCreationDelegate(connection);
