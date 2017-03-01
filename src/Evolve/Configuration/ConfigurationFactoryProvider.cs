d781357264633fe712342e04b6a1f0b69aec1400
Philippe Lécaillon <lecaillon@hotmail.com>
2017-03-01 22:31:28 +0100

    ConfigurationFactoryProvider implementation

--- src/Evolve/Configuration/ConfigurationFactoryProvider.cs
+++ src/Evolve/Configuration/ConfigurationFactoryProvider.cs
@@ -1,21 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using Evolve.Utilities;
 
 namespace Evolve.Configuration
 {
     public static class ConfigurationFactoryProvider
     {
-        static readonly Dictionary<string, IConfigurationProvider> providers = new Dictionary<string, IConfigurationProvider>
+        private const string NotSupportedConfigurationFile = "Evolve only supports App.config, Web.config or .json files.";
+        private static readonly Dictionary<string, Func<IConfigurationProvider>> _providers = new Dictionary<string, Func<IConfigurationProvider>>
         {
 #if NET452
-            [".config"] = new AppConfigConfigurationProvider(),
+            [".config"] = () => new AppConfigConfigurationProvider(),
 #endif
-            [".json"] = new JsonConfigurationProvider(),
+            [".json"] = () => new JsonConfigurationProvider(),
         };
 
         public static IConfigurationProvider GetProvider(string evolveConfigurationPath)
         {
-            throw new NotImplementedException();
+            Check.FileExists(evolveConfigurationPath, nameof(evolveConfigurationPath));
+
+            string ext = Path.GetExtension(evolveConfigurationPath);
+            _providers.TryGetValue(ext, out Func<IConfigurationProvider> providerCreationDelegate);
+            if (providerCreationDelegate == null)
+            {
+                throw new EvolveConfigurationException(NotSupportedConfigurationFile);
+            }
+
+            return providerCreationDelegate();
         }
     }
 }
