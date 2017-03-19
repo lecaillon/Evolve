221a726701503a5f34ecceed9eadff00229ec1f7
Philippe Lécaillon <lecaillon@hotmail.com>
2017-03-19 23:21:54 +0100

    Change exception message format

--- src/Evolve/Exception/EvolveConfigurationException.cs
+++ src/Evolve/Exception/EvolveConfigurationException.cs
@@ -4,25 +4,8 @@ namespace Evolve
 {
     public class EvolveConfigurationException : EvolveException
     {
-        private const string EvolveConfigurationError = "Evolve configuration error: ";
+        public EvolveConfigurationException(string message) : base(message) { }
 
-        public EvolveConfigurationException(string message) : base(EvolveConfigurationError + FirstLetterToLower(message)) { }
-
-        public EvolveConfigurationException(string message, Exception innerException) : base(EvolveConfigurationError + FirstLetterToLower(message), innerException) { }
-
-        private static string FirstLetterToLower(string str)
-        {
-            if (str == null)
-            {
-                return "";
-            }
-
-            if (str.Length > 1)
-            {
-                return Char.ToLowerInvariant(str[0]) + str.Substring(1);
-            }
-
-            return str.ToLowerInvariant();
-        }
+        public EvolveConfigurationException(string message, Exception innerException) : base(message, innerException) { }
     }
 }
