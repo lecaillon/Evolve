030865cf921b6fb731d26bce98cc2f873d9eaff1
Philippe Lécaillon <lecaillon@hotmail.com>
2017-03-19 10:40:25 +0100

    Add FirstLetterToLower() to each exception message

--- src/Evolve/Exception/EvolveConfigurationException.cs
+++ src/Evolve/Exception/EvolveConfigurationException.cs
@@ -6,8 +6,23 @@ namespace Evolve
     {
         private const string EvolveConfigurationError = "Evolve configuration error: ";
 
-        public EvolveConfigurationException(string message) : base(EvolveConfigurationError + message) { }
+        public EvolveConfigurationException(string message) : base(EvolveConfigurationError + FirstLetterToLower(message)) { }
 
-        public EvolveConfigurationException(string message, Exception innerException) : base(EvolveConfigurationError + message, innerException) { }
+        public EvolveConfigurationException(string message, Exception innerException) : base(EvolveConfigurationError + FirstLetterToLower(message), innerException) { }
+
+        private static string FirstLetterToLower(string str)
+        {
+            if (str == null)
+            {
+                return "";
+            }
+
+            if (str.Length > 1)
+            {
+                return Char.ToLowerInvariant(str[0]) + str.Substring(1);
+            }
+
+            return str.ToLowerInvariant();
+        }
     }
 }
