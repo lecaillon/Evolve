9cf9a2dd16d210fa2a7c111211d5f78f2a4a169d
lecaillon <lecaillon@hotmail.com>
2017-04-15 19:29:40 +0200

    Remove .NET Core support and add documentation

--- src/Evolve/Driver/SystemDataSQLiteDriver.cs
+++ src/Evolve/Driver/SystemDataSQLiteDriver.cs
@@ -1,22 +1,13 @@
-﻿#if NETSTANDARD
+﻿#if NET
 
 namespace Evolve.Driver
 {
-    public class SystemDataSQLiteDriver : CoreReflectionBasedDriver
-    {
-        public const string DriverAssemblyName = "System.Data.SQLite";
-        public const string ConnectionTypeName = "System.Data.SQLite.SQLiteConnection";
-
-        public SystemDataSQLiteDriver(string depsFile, string nugetPackageDir) : base(DriverAssemblyName, ConnectionTypeName, depsFile, nugetPackageDir)
-        {
-        }
-    }
-}
-
-#else
-
-namespace Evolve.Driver
-{
+    /// <summary>
+    ///     System.Data.SQlite driver for projects targeting the .NET Framework.
+    /// </summary>
+    /// <remarks>
+    ///     This driver does not support .NET Core.
+    /// </remarks>
     public class SystemDataSQLiteDriver : NetReflectionBasedDriver
     {
         public const string DriverAssemblyName = "System.Data.SQLite";
