8914d498a3f024e7d33d556eb43bda13316d2083
lecaillon <lecaillon@hotmail.com>
2017-04-15 19:45:24 +0200

    Add .NET Standard/Core project support + refactoring

--- src/Evolve/Connection/CoreDriverConnectionProviderForNet.cs
+++ src/Evolve/Connection/CoreDriverConnectionProviderForNet.cs
@@ -1,4 +1,4 @@
-﻿#if NETSTANDARD
+﻿#if NET45
 
 using System;
 using System.Collections.Generic;
@@ -7,33 +7,33 @@ using Evolve.Driver;
 namespace Evolve.Connection
 {
     /// <summary>
-    ///     A startegy for obtaining a <see cref="WrappedConnection"/> from a .NET Core driver.
+    ///     A startegy for obtaining a <see cref="WrappedConnection"/> from a .NET Core driver in a MSBuild .NET context.
     /// </summary>
-    public class CoreDriverConnectionProvider : CoreDriverConnectionProviderBase
+    public class CoreDriverConnectionProviderForNet : CoreDriverConnectionProviderBase
     {
         /// <summary>
         ///     Map a driver name to its <see cref="IDriver"/> implementation.
         /// </summary>
         private readonly Dictionary<string, Func<string, string, IDriver>> _driversMap = new Dictionary<string, Func<string, string, IDriver>>
         {
-            ["microsoftdatasqlite"] = (depsFile, nugetPackageDir) => new CoreMicrosoftDataSqliteDriver(depsFile, nugetPackageDir),
-            ["microsoftsqlite"] = (depsFile, nugetPackageDir) => new CoreMicrosoftDataSqliteDriver(depsFile, nugetPackageDir),
+            ["microsoftdatasqlite"] = (depsFile, nugetPackageDir) => new CoreMicrosoftDataSqliteDriverForNet(depsFile, nugetPackageDir),
+            ["microsoftsqlite"] = (depsFile, nugetPackageDir) => new CoreMicrosoftDataSqliteDriverForNet(depsFile, nugetPackageDir),
 
-            ["npgsql"] = (depsFile, nugetPackageDir) => new CoreNpgsqlDriver(depsFile, nugetPackageDir),
-            ["postgresql"] = (depsFile, nugetPackageDir) => new CoreNpgsqlDriver(depsFile, nugetPackageDir),
+            ["npgsql"] = (depsFile, nugetPackageDir) => new CoreNpgsqlDriverForNet(depsFile, nugetPackageDir),
+            ["postgresql"] = (depsFile, nugetPackageDir) => new CoreNpgsqlDriverForNet(depsFile, nugetPackageDir),
 
-            ["sqlserver"] = (depsFile, nugetPackageDir) => new CoreSqlClientDriver(depsFile, nugetPackageDir),
-            ["sqlclient"] = (depsFile, nugetPackageDir) => new CoreSqlClientDriver(depsFile, nugetPackageDir),
+            ["sqlserver"] = (depsFile, nugetPackageDir) => new SqlClientDriver(),
+            ["sqlclient"] = (depsFile, nugetPackageDir) => new SqlClientDriver(),
         };
 
         /// <summary>
-        ///     Initializes a new instance of a <see cref="CoreDriverConnectionProvider"/> from the given <paramref name="driverName"/>.
+        ///     Initializes a new instance of a <see cref="CoreDriverConnectionProviderForNet"/> from the given <paramref name="driverName"/>.
         /// </summary>
         /// <param name="driverName"> Name of the driver to load. </param>
         /// <param name="connectionString"> Connection string used to initialised a database connection. </param>
         /// <param name="depsFile"> Dependency file of the project to migrate. </param>
         /// <param name="nugetPackageDir"> Path to the NuGet package folder. </param>
-        public CoreDriverConnectionProvider(string driverName, string connectionString, string depsFile, string nugetPackageDir)
+        public CoreDriverConnectionProviderForNet(string driverName, string connectionString, string depsFile, string nugetPackageDir) 
             : base(driverName, connectionString, depsFile, nugetPackageDir)
         {
         }
