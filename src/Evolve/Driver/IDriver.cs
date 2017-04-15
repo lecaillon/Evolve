d2c51941fce7827e9d864a986523a2e901f74016
lecaillon <lecaillon@hotmail.com>
2017-04-15 19:19:56 +0200

    Add documentation

--- src/Evolve/Driver/IDriver.cs
+++ src/Evolve/Driver/IDriver.cs
@@ -5,8 +5,10 @@ namespace Evolve.Driver
     public interface IDriver
     {
         /// <summary>
-        ///     Creates an IDbConnection object for the specific Driver.
+        ///     Creates an <see cref="IDbConnection"/> object for the specific driver.
         /// </summary>
+        /// <param name="connectionString"> The connection string used to initialize the IDbConnection. </param>
+        /// <returns> An initialized database connection. </returns>
         IDbConnection CreateConnection(string connectionString);
     }
 }
