015505293bd5a668edb42f5081d78dd084f96724
Philippe Lécaillon <lecaillon@hotmail.com>
2017-04-18 01:01:36 +0200

    Temp hard coding some Evolve configuration

--- src/Evolve/Configuration/JsonConfigurationProvider.cs
+++ src/Evolve/Configuration/JsonConfigurationProvider.cs
@@ -8,7 +8,13 @@ namespace Evolve.Configuration
     {
         protected override void Configure()
         {
+            _configuration.Command = CommandOptions.Erase;
 
+            //_configuration.Driver = "npgsql";
+            //_configuration.ConnectionString = "Server=127.0.0.1;Port=5432;Database=my_database;User Id=postgres;Password=Password12!;";
+
+            _configuration.Driver = "microsoftdatasqlite";
+            _configuration.ConnectionString = "Data Source=:memory:;";
         }
     }
 }
