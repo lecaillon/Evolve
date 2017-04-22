14c2b4c1cfcfd3f0d42e7412ef2181b428577a31
Philippe Lécaillon <lecaillon@hotmail.com>
2017-04-22 09:49:28 +0200

    Add CoreDriverForNet unit tests

--- test/Evolve.Test/Driver/SystemDataSQLiteDriverTest.cs
+++ test/Evolve.Test/Driver/SystemDataSQLiteDriverTest.cs
@@ -15,5 +15,40 @@ namespace Evolve.Test.Driver
 
             Assert.True(cnn.State == ConnectionState.Open);
         }
+
+        #region CoreDriverConnectionProviderForNet
+
+        [Fact(DisplayName = "CoreMicrosoftDataSqliteDriverForNet_works")]
+        public void CoreMicrosoftDataSqliteDriverForNet_works()
+        {
+            var driver = new CoreMicrosoftDataSqliteDriverForNet(TestContext.DriverResourcesDepsFile, TestContext.NugetPackageFolder);
+            var cnn = driver.CreateConnection("Data Source=:memory:");
+            cnn.Open();
+
+            Assert.True(cnn.State == ConnectionState.Open);
+        }
+
+        [Fact(DisplayName = "CoreNpgsqlDriverForNet_works")]
+        public void CoreNpgsqlDriverForNet_works()
+        {
+
+            var driver = new CoreNpgsqlDriverForNet(TestContext.DriverResourcesDepsFile, TestContext.NugetPackageFolder);
+            var cnn = driver.CreateConnection($"Server=127.0.0.1;Port=5432;Database=my_database;User Id=postgres;Password={TestContext.PgPassword};");
+            cnn.Open();
+
+            Assert.True(cnn.State == ConnectionState.Open);
+        }
+
+        [Fact(DisplayName = "SqlClientDriver_works")]
+        public void SqlClientDriver_works()
+        {
+            var driver = new SqlClientDriver();
+            var cnn = driver.CreateConnection("Server=127.0.0.1;Database=master;User Id=sa;Password=Password12!;");
+            cnn.Open();
+
+            Assert.True(cnn.State == ConnectionState.Open);
+        }
+
+        #endregion
     }
 }
--- test/Evolve.Test/TestContext.cs
+++ test/Evolve.Test/TestContext.cs
@@ -8,11 +8,24 @@ namespace Evolve.Test
     {
         static TestContext()
         {
+            ProjectFolder = Path.GetFullPath(Path.Combine(Path.GetDirectoryName(new Uri(typeof(TestContext).GetTypeInfo().Assembly.CodeBase).AbsolutePath), @"../../"));
+            DriverResourcesProjectFolder = Path.GetFullPath(Path.Combine(ProjectFolder, @"../Evolve.Core.Test.Resources.SupportedDrivers"));
+#if DEBUG
+            DriverResourcesDepsFile = Path.Combine(DriverResourcesProjectFolder, @"bin/Debug/netcoreapp1.1/Evolve.Core.Test.Resources.SupportedDrivers.deps.json");
+#else
+            DriverResourcesDepsFile = Path.Combine(DriverResourcesProjectFolder, @"bin/Release/netcoreapp1.1/Evolve.Core.Test.Resources.SupportedDrivers.deps.json");
+#endif
             ResourcesFolder = Path.Combine(Path.GetDirectoryName(new Uri(Assembly.GetExecutingAssembly().CodeBase).AbsolutePath), "Resources");
             AppConfigPath = Path.Combine(ResourcesFolder, "App.config");
             WebConfigPath = Path.Combine(ResourcesFolder, "Web.config");
         }
 
+        public static string ProjectFolder { get; }
+        public static string DriverResourcesProjectFolder { get; }
+        public static string DriverResourcesDepsFile { get; }
+        public static string NugetPackageFolder => $@"{EnvHome}/.nuget/packages";
+        public static string PgPassword => Environment.GetEnvironmentVariable("PGPASSWORD") ?? "Password12!";
+        public static string EnvHome => Environment.GetEnvironmentVariable("USERPROFILE") ?? Environment.GetEnvironmentVariable("HOME");
         public static string ResourcesFolder { get; }
         public static string AppConfigPath { get; }
         public static string WebConfigPath { get; }
