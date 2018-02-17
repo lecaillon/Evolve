7099d330cbf5ac49b8b6b3fb00db4f4919c5c722
Philippe Lécaillon <lecaillon@hotmail.com>
2018-02-17 10:39:33 +0100

    Add SqlClient .NET 35 test project.

---
+++ test-package/Evolve.SqlClient.ConsoleApp35.Test/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Evolve.SqlClient.ConsoleApp35.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
