26315a2fb29ee36cef2a8894deccad8f764d6b8c
lecaillon <lecaillon@hotmail.com>
2018-03-03 00:14:47 +0100

    Update sample

--- samples/AspNetCoreSample/Startup.cs
+++ samples/AspNetCoreSample/Startup.cs
@@ -1,21 +1,51 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Data.Sqlite;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace AspNetCoreSample
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        private readonly ILogger _logger;
+
+        public Startup(IConfiguration configuration, IHostingEnvironment env, ILogger<Startup> logger)
         {
             Configuration = configuration;
+            Environment = env;
+            _logger = logger;
         }
 
         public IConfiguration Configuration { get; }
 
+        public IHostingEnvironment Environment { get; }
+
         public void ConfigureServices(IServiceCollection services)
         {
+            if (Environment.IsProduction())
+            {
+                try
+                {
+                    var cnx = new SqliteConnection(Configuration.GetConnectionString("MyDatabase"));
+                    var evolve = new Evolve.Evolve("evolve.json", cnx, msg => _logger.LogInformation(msg))
+                    {
+                        Locations = new List<string> { "scripts/migrations" },
+                        IsEraseDisabled = true,
+                    };
+
+                    evolve.Migrate();
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogCritical("Database migration failed.", ex);
+                    throw;
+                }
+            }
+
             services.AddMvc();
         }
 
