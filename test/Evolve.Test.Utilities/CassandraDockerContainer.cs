92a2dc5099f1d57f2c968a409d1254a292e7dd3b
Philippe <pvlerick@gmail.com>
2018-03-16 17:08:11 +0100

    Added Cassandra container and fixture

---
+++ test/Evolve.Test.Utilities/CassandraDockerContainer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Evolve.Test.Utilities
+{
+    public class CassandraDockerContainer
+    {
+        private DockerContainer _container;
+
+        public string Id => _container.Id;
+        public string ExposedPort => "9042";
+        public string HostPort => "9042";
+        public string Keyspace => "my_keyspace";
+        public TimeSpan DelayAfterStartup => TimeSpan.FromMinutes(1);
+
+        public bool Start(bool fromScratch = false)
+        {
+            _container = new DockerContainerBuilder(new DockerContainerBuilderOptions
+            {
+                FromImage = "cassandra",
+                Tag = "latest",
+                Name = "cassandra-evolve",
+                Env = new[] { "CASSANDRA_CLUSTER_NAME=evolve", "CASSANDRA_DC=dc1", "CASSANDRA_RACK=rack1" },
+                ExposedPort = $"{ExposedPort}/tcp",
+                HostPort = HostPort,
+                DelayAfterStartup = DelayAfterStartup,
+                RemovePreviousContainer = fromScratch
+            }).Build();
+
+            return _container.Start();
+        }
+        public void Remove() => _container.Remove();
+        public bool Stop() => _container.Stop();
+        public void Dispose() => _container?.Dispose();
+    }
+}
---
+++ test/Evolve.Test.Utilities/CassandraFixture.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Evolve.Test.Utilities
+{
+    public class CassandraFixture : IDisposable
+    {
+        public CassandraFixture()
+        {
+            Cassandra = new CassandraDockerContainer();
+        }
+
+        public void Start(bool fromScratch = false)
+        {
+            Cassandra.Start(fromScratch);
+        }
+
+        public CassandraDockerContainer Cassandra { get; }
+
+        public void Dispose() => Cassandra.Dispose();
+    }
+}
