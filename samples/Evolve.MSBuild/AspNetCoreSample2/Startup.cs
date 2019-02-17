32c02f2a6c1933b04846899b9ddf199f07eed283
Philippe Lécaillon <lecaillon@hotmail.com>
2019-02-17 23:51:19 +0100

    Add Evolve MSBuild sample

---
+++ samples/Evolve.MSBuild/AspNetCoreSample2/Startup.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AspNetCoreSample2
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+    }
+}
