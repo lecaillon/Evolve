94d07bef7d1165841b691a2e9c039ba8ccf945fd
Philippe Lécaillon <lecaillon@hotmail.com>
2019-11-17 23:45:21 +0100

    Add new Metadata UI type

---
+++ src/Evolve/Migration/MigrationMetadataUI.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Evolve.Migration
+{
+    public class MigrationMetadataUI
+    {
+        public MigrationMetadataUI(string? version, string description, string category)
+        {
+            Version = version;
+            Description = description;
+            Category = category;
+        }
+
+        public MigrationMetadataUI(MigrationMetadata migration)
+        {
+            Id = migration.Id;
+            Version = migration.Version?.Label;
+            Category = migration.Type.ToString();
+            Description = migration.Description;
+            InstalledBy = migration.InstalledBy;
+            InstalledOn = migration.InstalledOn;
+            Success = migration.Success;
+            Checksum = migration.Checksum;
+        }
+
+        public int? Id { get; set; }
+        public string? Version { get; set; }
+        public string? Category { get; set; }
+        public string? Description { get; set; }
+        public string? InstalledBy { get; set; }
+        public DateTime? InstalledOn { get; set; }
+        public bool? Success { get; set; }
+        public string? Checksum { get; set; }
+    }
+}
