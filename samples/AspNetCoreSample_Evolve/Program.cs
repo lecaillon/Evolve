7fd2ea20b47bd8ae71cd2fa394d89b8d7246d624
Philippe Lécaillon <lecaillon@hotmail.com>
2020-02-13 23:40:53 +0100

    Upgrade to net core 3.1 (#163)

--- samples/AspNetCoreSample_Evolve/Program.cs
+++ samples/AspNetCoreSample_Evolve/Program.cs
@@ -1,17 +1,96 @@
-﻿using Microsoft.AspNetCore;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Data.Sqlite;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Serilog;
 
 namespace AspNetCoreSample
 {
     public class Program
     {
-        public static void Main(string[] args)
+        private static readonly string EnvironmentName;
+        private static readonly IConfiguration Configuration;
+
+        static Program()
         {
-            CreateWebHostBuilder(args).Build().Run();
+            EnvironmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            Configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false)
+                .AddJsonFile($"appsettings.{EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables()
+                .Build();
+
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .CreateLogger();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+        public static int Main(string[] args)
+        {
+            try
+            {
+                Log.Information("Starting web host");
+                CreateHostBuilder(args).Build().Run();
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly");
+                return 1;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            MigrateDatabase();
+
+            return Host.CreateDefaultBuilder(args)
+                       .ConfigureAppConfiguration((hostingContext, config) =>
+                       {
+                           config.AddConfiguration(Configuration);
+                       })
+                       .ConfigureWebHostDefaults(webBuilder =>
+                       {
+                           webBuilder.UseStartup<Startup>();
+                       })
+                       .UseSerilog();
+        }
+
+        private static void MigrateDatabase()
+        {
+            // exclude db/datasets from production and staging environments
+            string location = EnvironmentName == Environments.Production || EnvironmentName == Environments.Staging
+                ? "db/migrations"
+                : "db";
+
+            try
+            {
+                var cnx = new SqliteConnection(Configuration.GetConnectionString("MyDatabase"));
+                var evolve = new Evolve.Evolve(cnx, msg => Log.Information(msg))
+                {
+                    Locations = new[] { location },
+                    IsEraseDisabled = true,
+                    Placeholders = new Dictionary<string, string>
+                    {
+                        ["${table4}"] = "table4"
+                    }
+                };
+
+                evolve.Migrate();
+            }
+            catch (Exception ex)
+            {
+                Log.Error("Database migration failed.", ex);
+                throw;
+            }
+        }
     }
 }
--- samples/AspNetCoreSample_MSBuild/Startup.cs
+++ samples/AspNetCoreSample_MSBuild/Startup.cs
@@ -2,20 +2,35 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace AspNetCoreSample2
 {
     public class Startup
     {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+
         }
 
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.Run(async (context) =>
+            if (env.IsDevelopment())
             {
-                await context.Response.WriteAsync("Hello World!");
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello World!");
+                });
             });
         }
     }
