d582d50f01d8dae8dbcea666013128093c009afa
Philippe Lécaillon <lecaillon@hotmail.com>
2020-02-13 23:32:50 +0100

    Update samples to .NET Core 3.1

--- samples/AspNetCoreSample_Evolve/Startup.cs
+++ samples/AspNetCoreSample_Evolve/Startup.cs
@@ -1,69 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.Data.Sqlite;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Hosting;
 
 namespace AspNetCoreSample
 {
     public class Startup
     {
-        private readonly ILogger _logger;
-
-        public Startup(IConfiguration configuration, IHostingEnvironment env, ILogger<Startup> logger)
-        {
-            Configuration = configuration;
-            Env = env;
-            _logger = logger;
-
-            MigrateDatabase(); // Evolve
-        }
-
-        public IConfiguration Configuration { get; }
-        public IHostingEnvironment Env { get; }
-
         public void ConfigureServices(IServiceCollection services)
         {
         }
 
-        public void Configure(IApplicationBuilder app)
+        public void Configure(IApplicationBuilder app, IHostEnvironment env)
         {
-            app.Run(async (context) =>
+            if (env.IsDevelopment())
             {
-                await context.Response.WriteAsync("Hello World!");
-            });
-        }
+                app.UseDeveloperExceptionPage();
+            }
 
-        private void MigrateDatabase()
-        {
-            string location = Env.IsProduction() || Env.IsStaging() // exclude db/datasets from production environment
-                ? "db/migrations"
-                : "db";
+            app.UseRouting();
 
-            try
+            app.UseEndpoints(endpoints =>
             {
-                var cnx = new SqliteConnection(Configuration.GetConnectionString("MyDatabase"));
-                var evolve = new Evolve.Evolve(cnx, msg => _logger.LogInformation(msg))
+                endpoints.MapGet("/", async context =>
                 {
-                    Locations = new[] { location },
-                    IsEraseDisabled = true,
-                    Placeholders = new Dictionary<string, string>
-                    {
-                        ["${table4}"] = "table4"
-                    }
-                };
-
-                evolve.Migrate();
-            }
-            catch (Exception ex)
-            {
-                _logger.LogCritical("Database migration failed.", ex);
-                throw;
-            }
+                    await context.Response.WriteAsync("Hello World!");
+                });
+            });
         }
     }
 }
--- samples/AspNetCoreSample_Evolve_EmbeddedResources/Startup.cs
+++ samples/AspNetCoreSample_Evolve_EmbeddedResources/Startup.cs
@@ -1,70 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.Data.Sqlite;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Hosting;
 
 namespace AspNetCoreSample3
 {
     public class Startup
     {
-        private readonly ILogger _logger;
-
-        public Startup(IConfiguration configuration, IHostingEnvironment env, ILogger<Startup> logger)
-        {
-            Configuration = configuration;
-            Env = env;
-            _logger = logger;
-
-            MigrateDatabase(); // Evolve
-        }
-
-        public IConfiguration Configuration { get; }
-        public IHostingEnvironment Env { get; }
-
         public void ConfigureServices(IServiceCollection services)
         {
         }
 
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostEnvironment env)
         {
-            app.Run(async (context) =>
+            if (env.IsDevelopment())
             {
-                await context.Response.WriteAsync("Hello World!");
-            });
-        }
+                app.UseDeveloperExceptionPage();
+            }
 
-        private void MigrateDatabase()
-        {
-            string filter = Env.IsProduction() || Env.IsStaging() // exclude db/datasets from production environment
-                ? "AspNetCoreSample3.db.migrations"
-                : "AspNetCoreSample3.db";
+            app.UseRouting();
 
-            try
+            app.UseEndpoints(endpoints =>
             {
-                var cnx = new SqliteConnection(Configuration.GetConnectionString("MyDatabase"));
-                var evolve = new Evolve.Evolve(cnx, msg => _logger.LogInformation(msg))
+                endpoints.MapGet("/", async context =>
                 {
-                    EmbeddedResourceAssemblies = new[] { typeof(Startup).Assembly },
-                    EmbeddedResourceFilters = new[] { filter },
-                    IsEraseDisabled = true,
-                    Placeholders = new Dictionary<string, string>
-                    {
-                        ["${table4}"] = "table4"
-                    }
-                };
-
-                evolve.Migrate();
-            }
-            catch (Exception ex)
-            {
-                _logger.LogCritical("Database migration failed.", ex);
-                throw;
-            }
+                    await context.Response.WriteAsync("Hello World!");
+                });
+            });
         }
     }
 }
