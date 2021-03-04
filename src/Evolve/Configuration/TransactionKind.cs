5549a93e154b936cb2ba644b3031d56fb0a5bd87
Philippe LECAILLON <plecaillon@vente-privee.com>
2021-03-04 19:48:08 +0100

    Add TransactionKind enum

---
+++ src/Evolve/Configuration/TransactionKind.cs
@@ -0,0 +1,19 @@
+﻿namespace Evolve.Configuration
+{
+    /// <summary>
+    ///     Define Evolve transaction mode.
+    /// </summary>
+    public enum TransactionKind
+    {
+        /// <summary>
+        ///     Commit each successful script and rollback only the one that fails (default mode).
+        /// </summary>
+        CommitEach,
+
+        /// <summary>
+        ///     Commit all the scripts at once and rollback them all if one fails.
+        ///     Either all succedeed or nothing is applied.
+        /// </summary>
+        CommitAll,
+    }
+}
