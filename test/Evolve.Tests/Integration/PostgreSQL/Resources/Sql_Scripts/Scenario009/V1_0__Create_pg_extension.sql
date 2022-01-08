50418a8a9ae45cc277359397906e6b5e49ba5a90
Philippe Lécaillon <lecaillon@hotmail.com>
2022-01-08 17:17:32 +0100

    Add Scenario009

---
+++ test/Evolve.Tests/Integration/PostgreSQL/Resources/Sql_Scripts/Scenario009/V1_0__Create_pg_extension.sql
@@ -0,0 +1,7 @@
+﻿create extension if not exists citext;
+create domain non_empty_citext as citext check (value ~ '\S');
+create table scenario009.test (
+     did    integer PRIMARY KEY,
+     name   non_empty_citext
+);
+insert into scenario009.test values (1, 'PSG');
