4fd9e9a7e472775ed05246c6a4933b753cef4475
Philippe Lécaillon <lecaillon@hotmail.com>
2022-01-09 09:54:39 +0100

    Remove Scenario009

--- test/Evolve.Tests/Integration/PostgreSQL/Resources/Sql_Scripts/Scenario009/V1_0__Create_pg_extension.sql
+++
@@ -1,7 +0,0 @@
-﻿create extension if not exists citext WITH SCHEMA ${schema};
-create domain ${schema}.non_empty_citext as ${schema}.citext check (value ~ '\S');
-create table ${schema}.test (
-     did    integer PRIMARY KEY,
-     name   ${schema}.non_empty_citext
-);
-insert into ${schema}.test values (1, 'PSG');
